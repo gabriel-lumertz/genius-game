2356a1fed31d5414aac8593e0ca0b99e1dd01031
Gabriel <glumertz10@gmail.com>
2022-01-16 01:47:35 -0300

    parte 5

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ let lightColor = (element, number) => {
 let checkOrder = () => {
     for(let i in clickedOrder) {
         if(clickedOrder[i] != order[i]) {
-            lose()
+            gameOver()
             break
         }
     }
@@ -57,7 +57,48 @@ let click = (color) => {
 
     setTimeout(() => {
         creatColorElement(color).classList.remove('.selected')
-    })
-
-    checkOrder()
+        checkOrder()
+    }, 250)
 }
+
+// criar cor
+let creatColorElement = (color) => {
+    if(color == 0) {
+        return green
+    } else if(color == 1) {
+        return red
+    } else if(color == 2) {
+        return yellow
+    } else if(color == 3) {
+        return blue
+    }
+}
+
+// criar próxima rodada
+let nextLevel = () => {
+    score++
+    shuffleOrder()
+}
+
+// encerrar jogo
+let gameOver = () => {
+    alert(`Pontução: ${score}!\nVocê perdeu\nClique em OK para iniciar um novo jogo.`)
+    order = []
+    clickedOrder = []
+
+    // playGame()
+}
+
+// iniciar jogo
+let playGame = () => {
+    alert('Bem-vindo ao Genius')
+    score = 0
+    nextLevel()
+}
+
+green.addEventListener('click', click(0))
+red.addEventListener('click', click(1))
+yellow.addEventListener('click', click(2))
+blue.addEventListener('click', click(3))
+
+playGame()
